fb8290855c6964736b5f3291fbdb165df8c71eca
V-Roger <roger.virgil@gmail.com>
2014-04-25 10:50:47 +0200

    Test Diff

--- protected/config/main.php
+++ protected/config/main.php
@@ -10,7 +10,7 @@ Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Junior Thomas',
+	'name'=>'Junior Virgil',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
