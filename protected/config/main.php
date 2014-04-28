8b5ded3112232cb68db6c2ec65956020062bf276
HugoDra <slaveak47@gmail.com>
2014-04-28 16:24:38 +0200

    test de push 3

--- protected/config/main.php
+++ protected/config/main.php
@@ -10,7 +10,7 @@ Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Junior Virgil',
+	'name'=>'Junior Hugo',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
