4e9c4026fb35d46a8cc1fe6c60d2a202562bb416
tombfx <tombfx@me.com>
2014-05-13 12:36:30 +0200

    Layout LoginJE

---
+++ themes/bootstrap/views/layouts/LoginJE.php
@@ -0,0 +1,40 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+
+	<?php Yii::app()->bootstrap->register(); ?>
+            <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
+
+</head>
+
+<body>
+
+<div class="container-fluid" id="page">
+        
+    <div class="row-fluid">
+    
+        <div class="span12">
+            <?php echo $content; ?>
+        </div>
+            
+
+    </div>
+        
+        <div class="clear"></div>
+    
+	<footer id="footer">
+		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		All Rights Reserved.<br/>
+		<?php echo Yii::powered(); ?>
+	</footer><!-- footer -->
+
+</div><!-- page -->
+
+</body>
+</html>
